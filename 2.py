aae34a3b72438d5bf0c432d800792a202b2b82d2
«Svetlana <passiong@mail.ru>
2022-04-20 21:39:19 +0500

    Задача 2

---
+++ 2.py
@@ -0,0 +1,12 @@
+# Найти максимальное из пяти чисел.
+a, b, c, d, e = 3, 43, 2, 78, 36
+max = a
+if max < b:
+    max = b
+if max < c:
+    max = c
+if max < d:
+    max = d
+if max < e:
+    max = e
+print(max)
