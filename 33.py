6d40bc6ccf561e534c42f618414cf005d7da3dbb
«Svetlana <passiong@mail.ru>
2022-05-05 19:01:19 +0500

    Задача 33 испр.

--- 33.py
+++ 33.py
@@ -22,3 +22,22 @@ while k >= 0:
         k=k-1
 file.close()
 
+# Второй многочлен для задачи 34
+
+d = int(input('Введите натуральную степень: '))
+coefficient1 = []
+for i in range(d+1):
+    coefficient1.append(randint(0, 100))
+print("Список коэффициентов: ", coefficient1)
+
+file = open ( "polynom1.txt", "w+" )
+while d >= 0:
+    for i in range(len(coefficient1)):
+        if d > 1 and coefficient1[i] != 0:
+            file.write(str(coefficient1[i]) + '*x^'+ str(d) + '+')
+        elif d == 1 and coefficient1[i] != 0:
+            file.write(str(coefficient1[i]) + '*x' + '+')
+        elif d == 0 and coefficient1[i] != 0:
+            file.write(str(coefficient1[i]) + ' = 0')
+        d=d-1
+file.close()
