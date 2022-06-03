3def0ccf9bd19076d161706d4373a63033c0b907
«Svetlana <passiong@mail.ru>
2022-06-03 14:40:18 +0500

    Задача 14_1

--- 14_1.py
+++ 14_1.py
@@ -1,4 +1,6 @@
 # Подсчитать сумму цифр в вещественном числе.
+# c помощью использования лямбд, filter, map, zip, enumerate, list comprehension
+
 n = input('Введите вещественное число: ')
 
 sum = sum(map(int, n.replace('.', '')))
